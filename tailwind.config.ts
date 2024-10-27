2f91614083e52ad1c0821224383c4dcd54243091
osamasabry9 <osamasabry324@gmail.com>
2024-10-27 18:05:45 +0400

    Update container width in globals.css and add new screen sizes in tailwind.config.ts
    Adjusted container width in globals.css for responsiveness and added new screen sizes in tailwind.config.ts for better layout control.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+      },
     },
   },
   plugins: [],
