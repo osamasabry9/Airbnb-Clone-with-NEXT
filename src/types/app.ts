5333d6e8b0e6e5316add31f7227e44d920ca39cb
osamasabry9 <osamasabry324@gmail.com>
2024-11-01 23:52:42 +0400

    Add search results API function
    Add a new function to fetch search results data from the API endpoint.
    This function will retrieve search data for display in the application.

--- src/types/app.ts
+++ src/types/app.ts
@@ -12,3 +12,33 @@ export type LiveItem = {
   title: string;
 };
 export type LiveData = LiveItem[];
+
+
+
+/*
+{
+    img: 'https://links.papareact.com/uz7',
+    location: 'Private room in center of London',
+    title: 'London Studio Apartments',
+    description: '4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine',
+    star: 3.8,
+    price: '£35 / night',
+    total: '£207 total',
+    long: -0.135638,
+    lat: 51.521916
+  },
+*/
+
+export type SearchResultItem = {
+  img: string;
+  location: string;
+  title: string;
+  description: string;
+  star: number;
+  price: string;
+  total: string;
+  long: number;
+  lat: number;
+}
+
+export type SearchResultData = SearchResultItem[]
